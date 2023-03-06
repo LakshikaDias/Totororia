b4057d79800a80749daa19553f44752fd7567679
LakshikaDias <lakshika9504@gmail.com>
2023-03-06 16:22:00 +0530

    navbar, login, and register pages were completed

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/system";
+import React from "react";
+import Navbar from "scenes/navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Box>
+        <Navbar />
+      </Box>
+    </div>
+  );
+};
+
+export default HomePage;
